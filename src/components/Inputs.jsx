adc9181af0554584a7c915876e0ab6e828ce8986
Anand Soni <aanandsoni50@gmail.com>
2023-04-05 19:23:26 +0530

    Minor changes

--- src/components/Inputs.jsx
+++ src/components/Inputs.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { UilSearch, UilLocationPinAlt } from '@iconscout/react-unicons';
-import { ToastContainer, toast } from 'react-toastify';
+import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 function Inputs({ setQuery, units, setUnits }) {
